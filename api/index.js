8512c405a9d322fa570dbe7bd53ca33a37a7863b
Erich Richter <erich.richter@gmail.com>
2021-08-04 10:50:45 -0700

    make it better

--- api/index.js
+++ api/index.js
@@ -53,6 +53,13 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen("5000", () => {
-  console.log("Backend is running");
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+
+  // app.listen("5000", () => {
+  //   console.log("Backend is running");
 });
