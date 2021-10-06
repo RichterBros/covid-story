d39afda47710662a4cb7a47348c6b4ff5f012645
Erich Richter <erich.richter@gmail.com>
2021-10-06 15:25:36 -0700

    modified for cloudinary

--- api/client/src/components/post/Post.jsx
+++ api/client/src/components/post/Post.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
 import "./post.css";
+import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
-  const PF = "https://covid-story.herokuapp.com/images/";
+  const PF = "http://localhost:5001/images/";
   return (
     <div className="post">
-      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
+      {post.photo && <img className="postImg" src={post.photo} alt="" />}
       <div className="postInfo">
         <div className="postCats">
           {post.categories.map((c) => (
