45278a641b773466c1ce234ea542e6d4d88ebb33
Erich Richter <erich.richter@gmail.com>
2021-10-07 10:15:57 -0700

    make it better

--- api/client/src/pages/settings/Settings.jsx
+++ api/client/src/pages/settings/Settings.jsx
@@ -2,7 +2,7 @@ import "./settings.css";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { useContext, useState } from "react";
 import { Context } from "../../context/Context";
-import axios from "axios";
+
 import { axiosInstance } from "../../config";
 
 export default function Settings() {
