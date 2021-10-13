6b037df91826188bcb68a9b9178728726bdacddd
Erich Richter <erich.richter@gmail.com>
2021-10-13 13:04:59 -0700

    add cors route

--- api/cors/index.js
+++ api/cors/index.js
@@ -1,6 +1,6 @@
 const cors = require("cors");
 const corsOptions = {
-  origin: "https://covid-story.herokuapp.com/",
+  origin: "https://covid-story.herokuapp.com/api/posts",
   optionsSuccessStatus: 200,
 };
 
