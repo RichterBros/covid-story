41797dc734ba8d414b0d327d40e1ed71596085d0
Erich Richter <erich.richter@gmail.com>
2023-02-22 13:47:36 -0800

    modify config

--- api/client/src/config.js
+++ api/client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://covid-story.herokuapp.com/api/",
+  baseURL: "https://covid-stories.onrender.com/api/",
 });
